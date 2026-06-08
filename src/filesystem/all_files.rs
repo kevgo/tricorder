39beeff4278d5e8c129702cae470b45d8c1b54a4
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-08 12:23:04 -0500

    Cargo.lock Cargo.toml all_files.rs

--- src/filesystem/all_files.rs
+++ src/filesystem/all_files.rs
@@ -1,27 +1,19 @@
-use std::env;
-use std::path::{Path, PathBuf};
+use ignore::Walk;
+use std::path::PathBuf;
 
 /// provides all files in the current working directory
 pub(crate) fn all_files() -> Vec<PathBuf> {
-    let root = env::current_dir().expect("cannot determine the current working directory");
     let mut result = Vec::new();
-    collect_files(&root, &mut result);
+    collect_files(&mut result);
     result
 }
 
-fn collect_files(dir: &Path, result: &mut Vec<PathBuf>) {
-    let Ok(entries) = dir.read_dir() else { return };
-    for entry in entries.flatten() {
-        // TODO: ignore .gitignored files
-        let Ok(file_type) = entry.file_type() else {
-            continue;
-        };
+fn collect_files(result: &mut Vec<PathBuf>) {
+    for entry in Walk::new("./") {
+        let Ok(entry) = entry else { continue };
         let path = entry.path();
-        // TODO: follow symlinks
-        if file_type.is_dir() {
-            collect_files(&path, result);
-        } else if file_type.is_file() {
-            result.push(path);
+        if path.is_file() {
+            result.push(path.to_owned());
         }
     }
 }
