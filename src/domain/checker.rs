efa7d0315bc2e6fe79d8d899602e20d91a42b9a8
Kevin Goslar <kevin.goslar@walmart.com>
2026-06-10 07:07:39 -0500

    Improve dev documentation

--- src/domain/checker.rs
+++ src/domain/checker.rs
@@ -4,7 +4,8 @@ use rta::applications::Apps;
 
 /// a checker that Tricorder can run
 pub trait Checker: Tool {
-    /// Provides the shell command to run this checker for all the stacks it supports.
+    /// Provides the shell command to run this for the given `PopulatedStack`.
+    /// The checker should only check the files in that instance, not all the files it is aware of.
     fn check_command(
         &self,
         stack: &PopulatedStack,
