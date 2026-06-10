4017b023974e41f1b32c5942dc714b245ed2f29b
Alsaeed Shawat <a0s1ryr@homeoffice.wal-mart.com>
2026-06-10 16:28:20 -0500

    UI fixes (#32)

--- UI/README.md
+++ UI/README.md
@@ -1,4 +1,4 @@
-# Web UI
+
 
 To run the UI demo:
 
--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,23 +1,24 @@
-import Navbar          from './components/Navbar'
-import Hero            from './components/Hero'
-import ScanSection     from './components/ScanSection'
-import TransformSection from './components/TransformSection'
-import LanguageSection from './components/LanguageSection'
-import PipelineSection from './components/PipelineSection'
-import ImpactSection   from './components/ImpactSection'
+import Navbar            from './components/Navbar'
+import Hero              from './components/Hero'
+import TricorderSection  from './components/TricorderSection'
+import TransformSection  from './components/TransformSection'
+import LanguageSection   from './components/LanguageSection'
+import PipelineSection   from './components/PipelineSection'
 import PlaygroundSection from './components/PlaygroundSection'
-import CTASection      from './components/CTASection'
+import CTASection        from './components/CTASection'
 
 export default function App() {
   return (
     <div style={{ fontFamily: 'Inter, sans-serif' }}>
       <Navbar />
       <Hero />
-      <ScanSection />
+      <TricorderSection />
       <TransformSection />
       <LanguageSection />
       <PipelineSection />
+
       {/* <ImpactSection /> */}
+
       <PlaygroundSection />
       <CTASection />
     </div>
