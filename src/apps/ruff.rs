e12d11f07d477a51e88bf95592d1ba81c00c3774
Kevin Goslar <kevin.goslar@walmart.com>
2026-06-10 15:49:51 -0500

    Ruff: check only the given files (#30)

--- src/apps/ruff.rs
+++ src/apps/ruff.rs
@@ -13,10 +13,16 @@ impl Tool for Ruff {
 
 impl Checker for Ruff {
     fn check_command(&self, stack: &PopulatedStack) -> Result<Option<conc::Executable>, UserError> {
+        let mut args = Vec::with_capacity(stack.files.len() + 1);
+        args.push(S("format"));
+        args.push(S("--check"));
+        for file in &stack.files {
+            args.push(file.to_string_lossy().to_string());
+        }
         get_check_command(&GetCheckCmdArgs {
             name: format!("{} ({})", &stack.stack.name(), self.name()),
             app: &rta::applications::Ruff {},
-            args: vec![S("format"), S("--check")],
+            args,
         })
     }
 }
