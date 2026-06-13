ed0ad1d5e319151a88949e46bf84071f9590f232
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-13 14:35:54 -0500

    Move the errors into the domain module (#45)

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,9 +1,11 @@
 mod checker;
+mod error;
 mod formatter;
 mod stack;
 mod tool;
 
 pub use checker::Checker;
+pub use error::{Result, UserError};
 pub use formatter::Formatter;
 pub use stack::{DetectedStack, Stack};
 pub use tool::Tool;
