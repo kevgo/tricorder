bf1ae5695f1808bdbc0afa167506beb854cf9b0c
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-17 06:05:38 -0500

    prettier.rs json.rs yml.rs

--- src/apps/prettier.rs
+++ src/apps/prettier.rs
@@ -1,5 +1,5 @@
 use crate::apps::{GetRTACmdArgs, get_rta_command};
-use crate::domain::{Checker, DetectedStack, Formatter, Tool, UserError};
+use crate::domain::{DetectedStack, Formatter, Tool, UserError};
 use big_s::S;
 
 pub struct Prettier;
@@ -7,24 +7,6 @@ pub struct Prettier;
 impl Tool for Prettier {
     fn name(&self) -> &'static str {
         "prettier"
-    }
-}
-
-impl Checker for Prettier {
-    fn check_commands(&self, stack: &DetectedStack) -> Result<Option<conc::Runnable>, UserError> {
-        let mut args: Vec<String> = Vec::with_capacity(stack.files.len() + 1);
-        args.push(S("--list-different"));
-        for stack_file in &stack.files {
-            let file_str = stack_file.to_string_lossy().to_string();
-            args.push(file_str);
-        }
-        let executable = get_rta_command(&GetRTACmdArgs {
-            name: format!("{} ({})", &stack.stack.name(), self.name()),
-            app: &rta::applications::PrettierStandalone {},
-            args,
-            version: None,
-        })?;
-        Ok(executable.map(conc::Runnable::Single))
     }
 }
 
