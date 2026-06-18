88d427ff41af375838e70deaf934561dc9c8d40c
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-18 07:32:08 -0500

    mod.rs pyright.rs

---
+++ src/apps/pyright.rs
@@ -0,0 +1,43 @@
+use crate::apps::{GetRTACmdArgs, get_rta_command};
+use crate::domain::{Checker, DetectedStack, DetectedStacks, StackType, Tool, UserError};
+use big_s::S;
+use std::fmt::Display;
+
+pub struct Pyright;
+
+impl Tool for Pyright {
+    fn is_enabled(&self, detected_stacks: &DetectedStacks) -> bool {
+        let Some(json_stack) = detected_stacks.get_stack(StackType::Json) else {
+            return false;
+        };
+        json_stack.files.contains("pyrightconfig.json")
+    }
+}
+
+impl Display for Pyright {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Pyright")
+    }
+}
+
+impl Checker for Pyright {
+    fn check_commands(&self, stack: &DetectedStack) -> Result<Option<conc::Runnable>, UserError> {
+        let mut args = Vec::with_capacity(stack.files.len() + 3);
+        args.push(S("run"));
+        args.push(S("--"));
+        args.push(S("pyright"));
+        for file in &stack.files {
+            args.push(file.to_string_lossy().to_string());
+        }
+        let executable = get_rta_command(&GetRTACmdArgs {
+            name: stack.stack.to_string(),
+            app: &rta::applications::Uv {},
+            args,
+            version: None,
+        })?;
+        let Some(executable) = executable else {
+            return Ok(None);
+        };
+        Ok(Some(conc::Runnable::Single(executable)))
+    }
+}
