8003e111cdb28f7772a3640710069aa2912dbce8
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-17 20:58:08 -0500

    Handle other files via an "other" stack (#89)

---
+++ src/stacks/unknown.rs
@@ -0,0 +1,45 @@
+use crate::domain::{Checker, Formatter, Stack, StackType};
+use std::path::Path;
+
+pub struct Unknown;
+
+impl Stack for Unknown {
+    fn stack_type(&self) -> StackType {
+        StackType::Unknown
+    }
+
+    fn has_file(&self, _file: &Path) -> bool {
+        true
+    }
+
+    fn checkers(&self) -> Vec<Box<dyn Checker>> {
+        vec![]
+    }
+
+    fn formatters(&self) -> Vec<Box<dyn Formatter>> {
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::Stack;
+    use crate::stacks::Unknown;
+    use maplit::hashmap;
+    use std::path::Path;
+
+    #[test]
+    fn has_file() {
+        let tests = hashmap! {
+            "main.toml" => true,
+            "src/dir/main.toml" => true,
+            "other.txt" => true,
+            ".prettierrc" => true,
+        };
+        let unknown = Unknown {};
+        for (give, want) in tests {
+            let have = unknown.has_file(Path::new(give));
+            assert_eq!(have, want, "{give:?} -> {have:?}");
+        }
+    }
+}
