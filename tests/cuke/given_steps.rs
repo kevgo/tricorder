97413cbf51237385b5f7523391a534d9509d65ff
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-20 14:30:14 -0500

    Extract Cucumber components into separate files (#117)

---
+++ tests/cuke/given_steps.rs
@@ -0,0 +1,81 @@
+use crate::world::{ExistingFile, TricorderWorld};
+use cucumber::gherkin::Step;
+use cucumber::given;
+use tokio::fs;
+use tokio::process::Command;
+
+#[given(expr = "a file {string} with content")]
+async fn a_file_with_content(world: &mut TricorderWorld, step: &Step, filename: String) {
+    let content = step.docstring.as_ref().unwrap();
+    let content = content.replace("\\t", "\t");
+    let content = content[1..].to_string();
+    let filepath = world.dir.join(&filename);
+    let parent = filepath.parent().unwrap();
+    if parent != world.dir {
+        fs::create_dir_all(parent)
+            .await
+            .unwrap_or_else(|_| panic!("cannot create parent '{}'", parent.display()));
+    }
+    fs::write(&filepath, content.as_bytes())
+        .await
+        .unwrap_or_else(|_| panic!("cannot write to file '{}'", filepath.display()));
+    world.original_files.push(ExistingFile {
+        name: filename,
+        content,
+    });
+}
+
+#[given(expr = "an executable file {string} with content")]
+async fn an_executable_file_with_content(
+    world: &mut TricorderWorld,
+    step: &Step,
+    filename: String,
+) {
+    let content = step.docstring.as_ref().unwrap()[1..].to_string();
+    let filepath = world.dir.join(&filename);
+    let parent = filepath.parent().unwrap();
+    if parent != world.dir {
+        fs::create_dir_all(parent)
+            .await
+            .unwrap_or_else(|_| panic!("cannot create parent '{}'", parent.display()));
+    }
+    fs::write(&filepath, content.as_bytes())
+        .await
+        .unwrap_or_else(|_| panic!("cannot write to file '{}'", filepath.display()));
+    #[cfg(unix)]
+    {
+        use std::os::unix::fs::PermissionsExt;
+        let mut perms = fs::metadata(&filepath).await.unwrap().permissions();
+        perms.set_mode(0o755);
+        fs::set_permissions(&filepath, perms).await.unwrap();
+    }
+    world.original_files.push(ExistingFile {
+        name: filename,
+        content,
+    });
+}
+
+#[given(expr = "I ran {string}")]
+async fn i_ran(world: &mut TricorderWorld, command: String) {
+    let mut args = command.split_ascii_whitespace();
+    let executable = args.next().expect("executable is required");
+    assert!(executable == "tools/rta", "can only execute 'tools/rta'");
+    let mut absolute_path = world.cwd.join("tools").join("rta");
+    if std::env::consts::OS == "windows" {
+        absolute_path.set_extension("exe");
+    }
+    let mut cmd = Command::new(absolute_path);
+    cmd.args(args);
+    cmd.current_dir(&world.dir);
+    let output = cmd
+        .output()
+        .await
+        .unwrap_or_else(|_| panic!("cannot find the '{executable}' executable"));
+    assert!(
+        output.status.success(),
+        "command failed with {}\n\nOUTPUT:\n{}{}",
+        output.status.code().unwrap(),
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr),
+    );
+}
---
+++ tests/cuke/world.rs
@@ -0,0 +1,56 @@
+use cucumber::World;
+use std::path::PathBuf;
+use std::process::Output;
+
+#[derive(Debug, World)]
+#[world(init = Self::new)]
+pub struct TricorderWorld {
+    /// need to hold on to this to keep the tempdir alive
+    _tempdir: tempfile::TempDir,
+
+    /// the directory containing the test files for the current scenario
+    pub dir: PathBuf,
+
+    /// the current working directory
+    pub cwd: PathBuf,
+
+    pub original_files: Vec<ExistingFile>,
+
+    /// the result of running Tricorder
+    pub output: Option<Output>,
+
+    /// path to the .feature file of the currently running scenario
+    pub feature_path: Option<PathBuf>,
+}
+
+impl TricorderWorld {
+    pub fn new() -> Self {
+        let tempdir = tempfile::tempdir().unwrap();
+        let random = rand::random_range(0..u64::MAX).to_string();
+        let dir = tempdir.path().join(random);
+        std::fs::create_dir(&dir).unwrap();
+        let cwd = std::env::current_dir().unwrap();
+        Self {
+            _tempdir: tempdir,
+            cwd,
+            dir,
+            original_files: Vec::new(),
+            output: None,
+            feature_path: None,
+        }
+    }
+
+    /// provides the exit code of the Atlanta run
+    pub fn exit_code(&self) -> i32 {
+        match &self.output {
+            Some(result) => result.status.code().unwrap(),
+            None => panic!(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ExistingFile {
+    pub name: String,
+    pub content: String,
+}
