0791669063e9098212f8f3c541c8cb0c659dd4d3
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-20 18:00:40 -0500

    fix.rs

--- src/commands/fix.rs
+++ src/commands/fix.rs
@@ -24,35 +24,69 @@ pub fn fix(args: &RunArgs) -> Result<ExitCode> {
     if runnables.is_empty() {
         return Ok(ExitCode::SUCCESS);
     }
+    let Runnables {
+        global,
+        stack_specific,
+    } = runnables;
     let show = conc::Show::from(args.show);
     let error_on_output = false;
     let stderr_to_stdout = true;
+    let exit_code = conc::run(conc::RunArgs {
+        runnables: global,
+        error_on_output,
+        stderr_to_stdout,
+        show,
+    });
+    if exit_code != ExitCode::SUCCESS {
+        return Ok(exit_code);
+    }
     Ok(conc::run(conc::RunArgs {
-        runnables,
+        runnables: stack_specific,
         error_on_output,
         show,
         stderr_to_stdout,
     }))
 }
 
-fn determine_runnables(stacks: &DetectedStacks) -> Result<Vec<conc::Runnable>> {
-    let mut runnables = Vec::new();
-
+fn determine_runnables(stacks: &DetectedStacks) -> Result<Runnables> {
     // global formatters
+    let mut global = Vec::new();
     if let Some(delete_empty_folders) = delete_empty_folders::format_command()? {
-        runnables.push(conc::Runnable::Single(delete_empty_folders));
+        global.push(conc::Runnable::Single(delete_empty_folders));
     }
 
     // stack-specific formatters
+    let mut stack_specific = Vec::new();
     for stack in stacks {
         for formatter in stack.stack.formatters() {
             if !formatter.is_enabled(stacks) {
                 continue;
             }
             if let Some(runnable) = formatter.format_command(stack)? {
-                runnables.push(runnable);
+                stack_specific.push(runnable);
             }
         }
     }
-    Ok(runnables)
+    Ok(Runnables {
+        global,
+        stack_specific,
+    })
+}
+
+struct Runnables {
+    /// formatters that affect all files
+    global: Vec<conc::Runnable>,
+
+    /// formatters that affect stack-specific files
+    stack_specific: Vec<conc::Runnable>,
+}
+
+impl Runnables {
+    pub fn is_empty(&self) -> bool {
+        self.global.is_empty() && self.stack_specific.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.global.len() + self.stack_specific.len()
+    }
 }
