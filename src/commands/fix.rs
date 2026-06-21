80439f1eb339ca007692709bf708385be5b24c37
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-20 20:42:27 -0500

    functional architecture and code sharing between fix and precommit (#121)

--- src/commands/fix.rs
+++ src/commands/fix.rs
@@ -6,53 +6,71 @@ use crate::stacks;
 use std::process::ExitCode;
 
 pub fn fix(args: &RunArgs) -> Result<ExitCode> {
+    let Runnables {
+        global,
+        stack_specific,
+    } = determine_runnables(args)?;
     let show = conc::Show::from(args.show);
     let error_on_output = false;
     let stderr_to_stdout = true;
-    let mut tool_count = 0;
+    let exit_code = conc::run(conc::RunArgs {
+        runnables: global,
+        error_on_output,
+        stderr_to_stdout,
+        show,
+    });
+    if exit_code != ExitCode::SUCCESS {
+        return Ok(exit_code);
+    }
+    let exit_code = conc::run(conc::RunArgs {
+        runnables: stack_specific,
+        error_on_output,
+        show,
+        stderr_to_stdout,
+    });
+    Ok(exit_code)
+}
 
-    // Run the global formatters because they apply to all files
-    // and can therefore interfere with the other formatters.
-    if let Some(delete_empty_folders) = delete_empty_folders::format_command()? {
-        tool_count += 1;
-        let exit_code = conc::run(conc::RunArgs {
-            runnables: vec![conc::Runnable::Single(delete_empty_folders)],
-            error_on_output,
-            show,
-            stderr_to_stdout,
-        });
-        if exit_code != ExitCode::SUCCESS {
-            return Ok(exit_code);
-        }
+pub fn determine_runnables(args: &RunArgs) -> Result<Runnables> {
+    // step 1: load the config
+
+    // step 2: discover the stacks
+    let stacks = stacks::discover();
+    if args.show == Show::All {
+        print_metadata(&stacks);
     }
 
-    // run the other formatters
-    let stacks = stacks::discover();
-    let mut runnables = Vec::new();
+    // step 3.1 global formatters
+    let mut global = Vec::new();
+    if let Some(delete_empty_folders) = delete_empty_folders::format_command()? {
+        global.push(conc::Runnable::Single(delete_empty_folders));
+    }
+
+    // step 3.2 stack-specific formatters
+    let mut stack_specific = Vec::new();
     for stack in &stacks {
         for formatter in stack.stack.formatters() {
             if !formatter.is_enabled(&stacks) {
                 continue;
             }
-            let Some(runnable) = formatter.format_command(stack)? else {
-                // this app is not available for this platform --> don't run it
-                continue;
-            };
-            tool_count += 1;
-            runnables.push(runnable);
+            if let Some(runnable) = formatter.format_command(stack)? {
+                stack_specific.push(runnable);
+            }
         }
     }
     if args.show == Show::All {
-        print_metadata(&stacks);
-        eprintln!("running {tool_count} tools");
+        eprintln!("running {} tools", global.len() + stack_specific.len());
     }
-    if runnables.is_empty() {
-        return Ok(ExitCode::SUCCESS);
-    }
-    Ok(conc::run(conc::RunArgs {
-        runnables,
-        error_on_output,
-        show,
-        stderr_to_stdout,
-    }))
+    Ok(Runnables {
+        global,
+        stack_specific,
+    })
+}
+
+pub struct Runnables {
+    /// formatters that affect all files
+    pub global: Vec<conc::Runnable>,
+
+    /// formatters that affect stack-specific files
+    pub stack_specific: Vec<conc::Runnable>,
 }
