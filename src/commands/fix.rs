217d00a83965337a788f0a1cd7d9f45160cbc23f
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-22 10:00:08 -0500

    fix.rs

--- src/commands/fix.rs
+++ src/commands/fix.rs
@@ -59,8 +59,7 @@ pub fn determine_runnables(args: &RunArgs) -> Result<Runnables> {
             if !formatter.is_enabled(&stacks) {
                 continue;
             }
-            let formatter_executables = formatter.format_commands(stack)?;
-            stack_executables.extend(formatter_executables);
+            stack_executables.extend(formatter.format_commands(stack)?);
         }
     }
 
