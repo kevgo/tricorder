7739be4c88a8e3fc7d79480437feee89fa56ce5b
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-22 10:27:43 -0500

    Cargo.lock Cargo.toml stack_type.rs

--- src/domain/stack_type.rs
+++ src/domain/stack_type.rs
@@ -1,9 +1,8 @@
-use serde::Deserialize;
+use serde_with::DeserializeFromStr;
 use std::fmt::Display;
-use std::str::FromStr;
 use strum::EnumString;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, EnumString)]
+#[derive(Clone, Copy, Debug, DeserializeFromStr, PartialEq, Eq, Hash, EnumString)]
 #[strum(ascii_case_insensitive)]
 pub enum StackType {
     Css,
@@ -18,31 +17,6 @@ pub enum StackType {
     Typescript,
     Unknown,
     Yml,
-}
-
-impl<'de> Deserialize<'de> for StackType {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let s = String::deserialize(deserializer)?;
-        StackType::from_str(&s).map_err(|_| {
-            serde::de::Error::unknown_variant(
-                &s,
-                &[
-                    "Css",
-                    "Cucumber",
-                    "Go",
-                    "Java",
-                    "Json",
-                    "Markdown",
-                    "Python",
-                    "Sql",
-                    "Toml",
-                    "Typescript",
-                    "Unknown",
-                    "Yml",
-                ],
-            )
-        })
-    }
 }
 
 impl Display for StackType {
