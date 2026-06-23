7ffa1bb166e0dda92fe12a68bb49fd37bbecac8d
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-23 07:10:42 -0500

    Shorten names (#134)

--- src/domain/lint.rs
+++ src/domain/lint.rs
@@ -1,10 +1,10 @@
 use crate::domain::{DetectedStack, Tool, UserError};
 
-/// a linter that Tricorder can run
+/// a lint that Tricorder can run
 pub trait Lint: Tool {
-    /// Provides the shell command to run this linter for the given `PopulatedStack`.
-    /// The linter can look at the files in `all_stacks` to determine if it should run.
-    /// If it runs, the linter should only verify the files in the given `DetectedStack`,
+    /// Provides the shell command to run this lint for the given `PopulatedStack`.
+    /// The lint can look at the files in `all_stacks` to determine if it should run.
+    /// If it runs, the lint should only verify the files in the given `DetectedStack`,
     /// not find all the files to lint by itself.
     /// This is for performance reasons. Finding files requires expensive OS calls.
     fn lint_commands(&self, stack: &DetectedStack) -> Result<Option<conc::Runnable>, UserError>;
--- src/domain/tool.rs
+++ src/domain/tool.rs
@@ -1,7 +1,7 @@
 use crate::domain::DetectedStacks;
 use std::fmt::Display;
 
-/// a tool (linter or fixer) that Tricorder can run
+/// a tool (lint or fix) that Tricorder can run
 pub trait Tool: Display {
     fn is_enabled(&self, detected_stacks: &DetectedStacks) -> bool;
 }
