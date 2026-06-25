bf9db1c34bb541ced26caf913b3264d76226703c
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-24 20:26:46 -0500

    fix.rs

--- src/domain/fix.rs
+++ src/domain/fix.rs
@@ -9,7 +9,8 @@ pub trait Fix: Tool {
     /// This allows running all fixes in parallel.
     fn fix_commands(&self, stack: &DetectedStack) -> Result<Vec<conc::Executable>, UserError>;
 
-    /// Provides the shell command to make this tool fix the given `PopulatedStack` in an advanced way that is unsafe.
+    /// Provides the shell command to make this tool fix the given `PopulatedStack`
+    /// in an advanced way that requires review.
     /// The fix can look at the files in `all_stacks` to determine if it should run.
     /// If it runs, the fix should only format the files in the given `DetectedStack`,
     /// not find all the files to fix by itself.
