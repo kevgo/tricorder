47f1504b5962e598b3da0e59d3ce6b2e62559957
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-25 20:57:25 -0500

    ci.rs

--- src/commands/ci.rs
+++ src/commands/ci.rs
@@ -5,7 +5,6 @@ use std::process::{Command, ExitCode};
 
 pub fn ci(args: &RunArgs) -> Result<ExitCode> {
     let before_diff = git_diff()?;
-    let before_status = git_status()?;
 
     let exit_code = pitstop(args)?;
     if exit_code != ExitCode::SUCCESS {
@@ -13,9 +12,8 @@ pub fn ci(args: &RunArgs) -> Result<ExitCode> {
     }
 
     let after_diff = git_diff()?;
-    let after_status = git_status()?;
 
-    if (before_diff != after_diff) || (before_status != after_status) {
+    if before_diff != after_diff {
         return Err(UserError::CiUnformatted { diff: after_diff });
     }
 
@@ -28,17 +26,6 @@ fn git_diff() -> Result<Vec<u8>> {
         .arg("color.ui=always")
         .arg("diff")
         .arg("HEAD")
-        .output()
-        .map_err(|err| UserError::CannotRunGit {
-            msg: err.to_string(),
-        })?;
-    Ok(output.stdout)
-}
-
-fn git_status() -> Result<Vec<u8>> {
-    let output = Command::new("git")
-        .arg("status")
-        .arg("--porcelain")
         .output()
         .map_err(|err| UserError::CannotRunGit {
             msg: err.to_string(),
