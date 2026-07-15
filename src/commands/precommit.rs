0fa7ba9c02896f872ff8d61b984b0adce1796d95
Kevin Goslar <kevin.goslar@gmail.com>
2026-07-14 19:09:24 -0500

    test

--- src/commands/precommit.rs
+++ src/commands/precommit.rs
@@ -1,8 +1,11 @@
+use ahash::AHashMap;
+
+use crate::apps::delete_empty_folders;
 use crate::cli::input::{self, RunArgs};
 use crate::cli::output::print_metadata;
-use crate::commands::fix::{Runnables, determine_fixes};
-use crate::config::Config;
-use crate::domain::Result;
+use crate::commands::fix::Runnables;
+use crate::config::{Config, CustomFix};
+use crate::domain::{DetectedStacks, Result, StackType};
 use crate::stacks;
 use std::process::ExitCode;
 
@@ -14,13 +17,13 @@ pub fn precommit(args: &RunArgs) -> Result<ExitCode> {
     let stderr_to_stdout = true;
 
     // step 2: discover the stacks
-    let stacks = stacks::discover_staged();
+    let staged_stacks = stacks::discover_staged();
     if show == conc::Show::All {
-        print_metadata(&stacks);
+        print_metadata(&staged_stacks);
     }
 
     // step 3: discover all runnables
-    let runnables = determine_fixes(config.custom_fixes, &stacks)?;
+    let runnables = determine_fixes_precommit(config.custom_fixes, &staged_stacks)?;
     if show == conc::Show::All {
         eprintln!("running {} tools", runnables.len());
     }
@@ -45,4 +48,61 @@ pub fn precommit(args: &RunArgs) -> Result<ExitCode> {
         stderr_to_stdout,
     });
     Ok(ExitCode::SUCCESS)
+}
+
+/// determines the fixes to run in the precommit command
+///
+/// The `staged_stacks` argument are the stacks that are currently staged in the git repository,
+/// not all stacks that exist in the workspace.
+pub fn determine_fixes_precommit(
+    custom_fixes: Option<Vec<CustomFix>>,
+    staged_stacks: &DetectedStacks,
+) -> Result<Runnables> {
+    // step 3 global fixes
+    let mut global = Vec::new();
+    if let Some(delete_empty_folders) = delete_empty_folders::format_command()? {
+        global.push(delete_empty_folders);
+    }
+
+    // step 4 stack-specific fixes
+    let mut stack_executables: AHashMap<StackType, Vec<conc::Executable>> = AHashMap::new();
+    for stack in staged_stacks {
+        let stack_executables = stack_executables
+            .entry(stack.stack.stack_type())
+            .or_default();
+        for fix in stack.stack.fixes() {
+            if !fix.is_enabled(stacks) {
+                continue;
+            }
+            stack_executables.extend(fix.fix_commands(stack)?);
+        }
+    }
+
+    // step 5 custom fixes
+    if let Some(custom_fixes) = custom_fixes {
+        for fix in custom_fixes {
+            let executable = conc::Executable {
+                name: fix.name.unwrap_or_else(|| fix.command.clone()),
+                command: conc::shell_command(&fix.command),
+            };
+            if let Some(stack) = fix.stack {
+                let stack_executables = stack_executables.entry(stack).or_default();
+                stack_executables.push(executable);
+            } else {
+                global.push(executable);
+            }
+        }
+    }
+
+    // step 6: convert to runnables and return
+    let mut stack_specific = Vec::new();
+    for (_stack_type, stack_executables) in stack_executables {
+        if !stack_executables.is_empty() {
+            stack_specific.push(conc::Runnable::Sequence(stack_executables));
+        }
+    }
+    Ok(Runnables {
+        global: conc::Runnable::Sequence(global),
+        stack_specific,
+    })
 }
