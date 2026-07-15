56a6e9b2a21a2e5bba25cfa46d47bef3a13e8275
Kevin Goslar <kevin.goslar@gmail.com>
2026-07-15 05:58:08 -0500

    precommit.rs

--- src/commands/precommit.rs
+++ src/commands/precommit.rs
@@ -76,10 +76,7 @@ pub fn determine_precommit_fixes(
                 EnabledWhen::FilePresent {
                     filename,
                     stack_type: _,
-                } => {
-                    let file = Path::new(&filename);
-                    file.exists()
-                }
+                } => Path::new(&filename).exists(),
             };
             if enabled {
                 stack_executables.extend(fix.fix_commands(stack)?);
