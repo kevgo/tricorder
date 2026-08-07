399525311901544e4892ec5c1280a4acefd09802
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-07 08:24:33 -0500

    files.rs normalized_path.rs

--- src/domain/files.rs
+++ src/domain/files.rs
@@ -12,8 +12,8 @@ impl Files {
 
     #[must_use]
     pub fn contains(&self, file: &str) -> bool {
-        for file in self.0 {
-            if file.as_ref() == file {
+        for self_file in &self.0 {
+            if self_file.as_ref() == file {
                 return true;
             }
         }
--- src/domain/normalized_path.rs
+++ src/domain/normalized_path.rs
@@ -1,23 +1,53 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
-/// A path that
+/// A path without a leading "./"
 #[derive(Debug, PartialEq, Eq)]
-pub struct NormalizedPath(PathBuf);
+pub struct NormalizedPath(String);
 
-impl AsRef<Path> for NormalizedPath {
-    fn as_ref(&self) -> &Path {
+impl AsRef<str> for NormalizedPath {
+    fn as_ref(&self) -> &str {
         &self.0
-    }
-}
-
-impl From<NormalizedPath> for PathBuf {
-    fn from(path: NormalizedPath) -> Self {
-        path.0
     }
 }
 
 impl From<&Path> for NormalizedPath {
     fn from(path: &Path) -> Self {
-        Self(path.strip_prefix("./").unwrap_or(path).to_path_buf())
+        let path = path.to_string_lossy();
+        let stripped = path.strip_prefix("./").unwrap_or(path.as_ref());
+        Self(stripped.to_string())
+    }
+}
+
+impl From<&str> for NormalizedPath {
+    fn from(path: &str) -> Self {
+        let stripped = path.strip_prefix("./").unwrap_or(path);
+        Self(stripped.to_string())
+    }
+}
+
+impl From<NormalizedPath> for String {
+    fn from(path: NormalizedPath) -> Self {
+        path.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_path() {
+        let tests = maplit::hashmap! {
+            "./test.txt" => "test.txt",
+            "test.txt" => "test.txt",
+            "/test.txt" => "/test.txt",
+            "//test.txt" => "//test.txt",
+            "test.txt/" => "test.txt/",
+            "test.txt/test.txt" => "test.txt/test.txt",
+        };
+        for (give, want) in tests {
+            let have = NormalizedPath::from(give);
+            assert_eq!(have, want.into());
+        }
     }
 }
