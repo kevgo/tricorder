330c19acfeeb66abf96d3d73cb1e07d047e00627
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-13 07:21:22 -0500

    then_steps.rs

--- tests/cuke/then_steps.rs
+++ tests/cuke/then_steps.rs
@@ -13,7 +13,7 @@ use tokio::process::Command;
 async fn all_files_unchanged(world: &mut TricorderWorld) {
     for original in &world.original_files {
         let filepath = world.dir.join(&original.name);
-        let have = fs::read_to_string(filepath).await.
+        let have = fs::read_to_string(filepath).await.unwrap();
             panic!(
                 "cannot read file '{}', which should still exist",
                 original.name
