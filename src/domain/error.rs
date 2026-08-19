a88d1237d8ea1a21f9828130782de988dc2c4a94
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-19 07:11:04 -0500

    error.rs ignores.rs

--- src/domain/error.rs
+++ src/domain/error.rs
@@ -8,23 +8,65 @@ pub type Result<T> = core::result::Result<T, UserError>;
 #[derive(Debug, PartialEq)]
 #[allow(clippy::module_name_repetitions)]
 pub enum UserError {
-    CannotCanonicalizePath { path: PathBuf },
-    CannotCreateDirectory { path: PathBuf, err: String },
-    CannotDetermineCurrentDirectory { err: String },
-    CannotFindTricorderExecutable { path: PathBuf, err: String },
-    CannotReadFileMetadata { path: PathBuf, err: String },
-    CannotWriteFile { path: String, err: String },
+    CannotCanonicalizePath {
+        path: PathBuf,
+    },
+    CannotCreateDirectory {
+        path: PathBuf,
+        err: String,
+    },
+    CannotDetermineCurrentDirectory {
+        err: String,
+    },
+    CannotFindTricorderExecutable {
+        path: PathBuf,
+        err: String,
+    },
+    CannotReadFileMetadata {
+        path: PathBuf,
+        err: String,
+    },
+    CannotWriteFile {
+        path: String,
+        err: String,
+    },
     ArgvIsEmpty,
-    CannotRunGit { msg: String },
-    CannotRunRipgrep { msg: String },
-    CannotSetFilePermissions { path: PathBuf, err: String },
-    CiUnformatted { diff: Vec<u8> },
-    Cli { msg: String },
-    Config { msg: String },
-    ExecutableNotFound { path: PathBuf },
+    CannotRunGit {
+        msg: String,
+    },
+    CannotRunRipgrep {
+        msg: String,
+    },
+    CannotSetFilePermissions {
+        path: PathBuf,
+        err: String,
+    },
+    CiUnformatted {
+        diff: Vec<u8>,
+    },
+    Cli {
+        msg: String,
+    },
+    ConfigCannotParse {
+        filename: String,
+        err: String,
+    },
+    ConfigCannotRead {
+        filename: String,
+        err: String,
+    },
+    ConfigInvalidIgnorePattern {
+        pattern: Option<String>,
+        err: String,
+    },
+    ExecutableNotFound {
+        path: PathBuf,
+    },
     NoGitRepository,
     NotMainGitWorktree,
-    Rta { err: rta::error::UserError },
+    Rta {
+        err: rta::error::UserError,
+    },
 }
 
 impl UserError {
@@ -59,7 +101,21 @@ impl UserError {
                 let _ = std::io::stdout().write_all(&diff);
             }
             // TODO: for CONFIG errors, print the config file path and then the message
-            UserError::Cli { msg } | UserError::Config { msg } => println!("{msg}"),
+            UserError::Cli { msg } => println!("{msg}"),
+            UserError::ConfigCannotParse { filename, err } => {
+                println!("config file {filename} seems to contain invalid JSON: {err}")
+            }
+            UserError::ConfigCannotRead { filename, err } => {
+                println!("cannot read config file {filename}: {err}")
+            }
+            UserError::ConfigInvalidIgnorePattern { pattern, err } => {
+                println!("config file contains invalid ignore pattern: {err}");
+                println!();
+                println!("The problematic pattern is: {pattern:?}");
+                println!();
+                println!("These must be valid gitignore patterns.");
+                println!("See https://git-scm.com/docs/gitignore#_pattern_format for the syntax.");
+            }
             UserError::ExecutableNotFound { path } => {
                 println!("executable not found: {}", path.display());
             }
