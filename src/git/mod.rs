1489752e2014d180e76484e8b88abe2216a773f1
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-22 13:02:27 -0500

    mod.rs

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -2,12 +2,11 @@ mod is_repo;
 mod porcelain;
 mod stage;
 mod status;
+#[cfg(test)]
+pub(crate) mod testing;
 mod uncommitted;
 
 pub use is_repo::is_repo;
 pub use stage::stage;
 pub use status::{StagedFiles, status};
 pub use uncommitted::uncommitted;
-
-#[cfg(test)]
-pub(crate) mod testing;
