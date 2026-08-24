4cd5a01e646cd4f0c233000abf408ccc460c4c2f
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-24 07:42:40 -0500

    testing.rs [skip ci]

--- src/git/testing.rs
+++ src/git/testing.rs
@@ -1,12 +1,14 @@
 use std::process::Command;
 use tempfile::TempDir;
 
+/// creates a new Git repository in a temp dir for testing
 pub fn git_repo() -> TempDir {
     let dir = TempDir::new().unwrap();
     git(&dir, &["init", "-q"]);
     dir
 }
 
+/// runs the given Git command in the given directory
 pub fn git(dir: &TempDir, args: &[&str]) {
     let output = Command::new("git")
         .args(args)
