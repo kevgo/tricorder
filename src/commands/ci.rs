eb5f1f36d04850dbc68a852ed6d4c07070920bf4
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-29 05:24:14 -0500

    ci.rs

--- src/commands/ci.rs
+++ src/commands/ci.rs
@@ -6,17 +6,28 @@ use std::path::Path;
 use std::process::ExitCode;
 
 pub fn ci(args: RunArgs) -> Result<ExitCode> {
-    let repo = Repo::load(None::<&Path>).ok_or(UserError::NoGitRepository)?;
-    let before_diff = repo.diff()?;
+    let repo = Repo::load(None::<&Path>);
+    let before_diff = if let Some(repo) = &repo {
+        Some(repo.diff()?)
+    } else {
+        None
+    };
 
     let exit_code = pitstop(&args.with_default_show(conc::Show::Names))?;
     if exit_code != ExitCode::SUCCESS {
         return Ok(exit_code);
     }
 
-    let after_diff = repo.diff()?;
+    let after_diff = if let Some(repo) = &repo {
+        Some(repo.diff()?)
+    } else {
+        None
+    };
 
-    if before_diff != after_diff {
+    if let Some(before_diff) = before_diff
+        && let Some(after_diff) = after_diff
+        && before_diff != after_diff
+    {
         return Err(UserError::CiUnformatted { diff: after_diff });
     }
 
