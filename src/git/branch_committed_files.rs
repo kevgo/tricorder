4ec18eabc5cf23ac8055c300f7675f0fff9fa6ce
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-31 07:46:26 -0500

    branch_committed_files.rs [skip ci]

--- src/git/branch_committed_files.rs
+++ src/git/branch_committed_files.rs
@@ -5,10 +5,15 @@ use crate::git::Repo;
 
 impl Repo {
     pub(crate) fn branch_committed_files(&self, merge_base: &str) -> Result<Vec<File>> {
-        let range = format!("{merge_base}...HEAD");
         Ok(self
             .git_command()
-            .args(["diff", "-z", "--name-only", "--diff-filter=ACMRT", &range])
+            .args([
+                "diff",
+                "-z",
+                "--name-only",
+                "--diff-filter=ACMRT",
+                &format!("{merge_base}...HEAD"),
+            ])
             .run_stdout_zero()?
             .lines()
             .map(File::from)
