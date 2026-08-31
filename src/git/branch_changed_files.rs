ba03acbddfe9ce42ff5ae4990051448c70b30161
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-31 07:37:51 -0500

    branch_changed_files.rs [skip ci]

--- src/git/branch_changed_files.rs
+++ src/git/branch_changed_files.rs
@@ -31,7 +31,7 @@ impl Repo {
     }
 }
 
-/// merges two file lists, then sorts, removes duplicates, and drops missing files
+/// provides the unique files that exist in both given lists
 fn unique_existing(repo: &Repo, mut files: Vec<File>, extra: Vec<File>) -> Vec<File> {
     files.extend(extra);
     files.sort();
