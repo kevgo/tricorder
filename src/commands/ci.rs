c9c3ac3b85cc76a347c79e7b3b3ef1e55b4a33fc
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-31 06:54:44 -0500

    Simplify option conditional (#271)

--- src/commands/ci.rs
+++ src/commands/ci.rs
@@ -6,23 +6,14 @@ use std::process::ExitCode;
 
 pub fn ci(args: RunArgs) -> Result<ExitCode> {
     let repo = Repo::load();
-    let before_diff = if let Some(repo) = &repo {
-        Some(repo.diff()?)
-    } else {
-        None
-    };
+    let before_diff = repo.as_ref().and_then(|repo| repo.diff().ok());
 
     let exit_code = pitstop(&args.with_default_show(conc::Show::Names))?;
     if exit_code != ExitCode::SUCCESS {
         return Ok(exit_code);
     }
 
-    let after_diff = if let Some(repo) = &repo {
-        Some(repo.diff()?)
-    } else {
-        None
-    };
-
+    let after_diff = repo.as_ref().and_then(|repo| repo.diff().ok());
     if let Some(before_diff) = before_diff
         && let Some(after_diff) = after_diff
         && before_diff != after_diff
