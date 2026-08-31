c10f976b97189e9a36a3644c7da9bf32b13778e8
Kevin Goslar <kevin.goslar@gmail.com>
2026-08-31 07:23:26 -0500

    pitstop.rs [skip ci]

--- src/commands/pitstop.rs
+++ src/commands/pitstop.rs
@@ -15,7 +15,7 @@ pub fn pitstop(args: &RunArgs) -> Result<ExitCode> {
     let stacks = match &repo {
         Some(repo) => match repo.branch_changed_files()? {
             Some(files) => stacks::from_files(&files, &ignores),
-            None => stacks::discover_all(&ignores),
+            None => DetectedStacks::new(vec![]),
         },
         None => stacks::discover_all(&ignores),
     };
