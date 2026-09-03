87c1710a0d6f6f9c7a255dc0abc824f487ad680c
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-03 08:37:21 -0500

    precommit.rs [skip ci]

--- src/commands/precommit.rs
+++ src/commands/precommit.rs
@@ -93,10 +93,8 @@ pub fn determine_precommit_fixes(
         let stack_type = staged_stack.stack.stack_type();
         let stack_config = config.stack_config(stack_type);
         let stack_executables = stacks_executables.entry(stack_type).or_default();
-        if let Some(override_fixes) = stack_config
-            .and_then(|stack_config| stack_config.fix.as_ref())
-            .and_then(|fix| fix.replace.as_ref())
-        {
+        let stack_fixes = stack_config.and_then(|stack_config| stack_config.fix.as_ref());
+        if let Some(override_fixes) = stack_fixes.and_then(|fix| fix.replace.as_ref()) {
             stack_executables.extend(override_fixes.iter().map(conc::Executable::from));
         } else {
             for default_fix in staged_stack.stack.fixes() {
@@ -105,11 +103,8 @@ pub fn determine_precommit_fixes(
                 }
             }
         }
-        if let Some(add) = stack_config
-            .and_then(|stack_config| stack_config.fix.as_ref())
-            .and_then(|fix| fix.add.as_ref())
-        {
-            stack_executables.extend(add.iter().map(conc::Executable::from));
+        if let Some(additions) = stack_fixes.and_then(|fix| fix.add.as_ref()) {
+            stack_executables.extend(additions.iter().map(conc::Executable::from));
         }
     }
 
