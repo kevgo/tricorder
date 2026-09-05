2697a123fd650a642a39edc0717a164cfafdcae4
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-04 20:35:53 -0500

    Improve Files::as_ref (#291)

--- src/domain/files.rs
+++ src/domain/files.rs
@@ -12,13 +12,9 @@ impl Files {
     }
 
     #[must_use]
-    pub fn contains(&self, file: &str) -> bool {
-        for self_file in &self.0 {
-            if self_file.as_ref() == file {
-                return true;
-            }
-        }
-        false
+    pub fn contains<AS: AsRef<str>>(&self, file: AS) -> bool {
+        let file = file.as_ref();
+        self.0.iter().any(|self_file| self_file.as_str() == file)
     }
 
     #[must_use]
