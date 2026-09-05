abb7fd7d239031f1e8c64173be0998820eb7309a
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-04 19:55:59 -0500

    gofumpt.rs

--- src/apps/gofumpt.rs
+++ src/apps/gofumpt.rs
@@ -1,4 +1,4 @@
-use crate::apps::{GetRTACmdArgs, filter_files, get_rta_command};
+use crate::apps::{GetRTACmdArgs, get_rta_command};
 use crate::config::Config;
 use crate::domain::{DetectedStack, EnabledWhen, Fix, Tool, UserError};
 use big_s::S;
@@ -24,11 +24,12 @@ impl Fix for Gofumpt {
         stack: &DetectedStack,
         config: &Config,
     ) -> Result<Vec<conc::Executable>, UserError> {
-        let filtered_files = filter_files(&stack.files, config, |apps| apps.gofumpt.as_ref());
-        let mut args = Vec::with_capacity(stack.files.len() - filtered_files.len() + 2);
+        let exclude_files = config.excluded_files_for_app(|apps| apps.gofumpt.as_ref());
+        let files = &stack.files.remove(&exclude_files);
+        let mut args = Vec::with_capacity(files.len() + 2);
         args.push(S("-l"));
         args.push(S("-w"));
-        args.extend(filtered_files.into_iter().map(Into::into));
+        args.extend(files.iter().map(ToString::to_string));
         let executable = get_rta_command(&GetRTACmdArgs {
             name: format!("fix {} ({self})", stack.stack),
             app: &rta::applications::Gofumpt {},
