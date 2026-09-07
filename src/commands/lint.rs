b62d9a4bc6d14c978f989fe645ea8813db8e4fb8
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-07 11:22:30 -0500

    lint.rs [skip ci]

--- src/commands/lint.rs
+++ src/commands/lint.rs
@@ -61,8 +61,8 @@ pub fn determine_lints(
             }
         } else {
             for default_lint in detected_stack.stack.lints() {
-                if default_lint.enabled_when().enabled_on_disk()
-                    && config.app_enabled(default_lint.as_ref())
+                if config.app_enabled(default_lint.as_ref())
+                    && default_lint.enabled_when().enabled_on_disk()
                     && let Some(executable) = default_lint.lint_commands(detected_stack, config)?
                 {
                     result.push(executable);
