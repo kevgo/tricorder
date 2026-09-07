71aea2f9aac88bbe988e501f524180265ff87c53
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-07 11:19:42 -0500

    fix.rs [skip ci]

--- src/commands/fix.rs
+++ src/commands/fix.rs
@@ -75,8 +75,8 @@ pub fn determine_fixes(config: &Config, detected_stacks: &DetectedStacks) -> Res
             stack_executables.extend(overrides.iter().map(conc::Executable::from));
         } else {
             for default_fix in detected_stack.stack.fixes() {
-                if default_fix.enabled_when().enabled_on_disk()
-                    && config.app_enabled(default_fix.as_ref())
+                if config.app_enabled(default_fix.as_ref())
+                    && default_fix.enabled_when().enabled_on_disk()
                 {
                     stack_executables.extend(default_fix.fix_commands(detected_stack, config)?);
                 }
