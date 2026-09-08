a0e17a8e63eb64ba19fcbbb8dc3eb4a280138cbe
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-08 06:40:52 -0500

    jitter.rs

--- tests/cuke/jitter.rs
+++ tests/cuke/jitter.rs
@@ -4,8 +4,6 @@ const JITTER_MIN_MS: u64 = 10;
 const JITTER_MAX_MS: u64 = 1000;
 
 pub(crate) async fn jitter() {
-    tokio::time::sleep(Duration::from_millis(rand::random_range(
-        JITTER_MIN_MS..JITTER_MAX_MS,
-    )))
-    .await;
+    let duration = Duration::from_millis(rand::random_range(JITTER_MIN_MS..JITTER_MAX_MS));
+    tokio::time::sleep(duration).await;
 }
