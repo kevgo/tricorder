911ec0cb0abb876a47e0861489e4ab5b4f5091a1
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-11 20:44:16 -0500

    delete_empty_folders.rs

--- src/apps/delete_empty_folders.rs
+++ src/apps/delete_empty_folders.rs
@@ -1,10 +1,7 @@
 use crate::apps::{GetRTACmdArgs, get_rta_command};
-use crate::config::Config;
 use crate::domain::{EnabledWhen, Result, Tool};
 use big_s::S;
 use std::fmt::Display;
-
-const DEFAULT_VERSION: &str = "0.0.2";
 
 pub struct DeleteEmptyFolders;
 
@@ -27,15 +24,13 @@ impl Display for DeleteEmptyFolders {
     }
 }
 
-pub fn format_command(config: &Config) -> Result<Option<conc::Executable>> {
+pub fn format_command() -> Result<Option<conc::Executable>> {
     get_rta_command(&GetRTACmdArgs {
         name: S("delete empty folders"),
         app: &rta::applications::DeleteEmptyFolders {},
         args: vec![],
         // TODO: if the run-that-app file defines a different version, use that one
         // and only fall back to the hard-coded one if the run-that-app file does not define a version
-        version: Some(rta::Version::from(DEFAULT_VERSION)),
+        version: None,
     })
 }
-
-fn version(config: &Config) -> Option<Version> {}
