a8b03b0fc449f2c1aa982fa07bb244fe149aaf62
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-12 15:18:20 -0500

    run-that-app-content-sorted.rs [skip ci]

--- tests/run-that-app-content-sorted.rs
+++ tests/run-that-app-content-sorted.rs
@@ -20,7 +20,7 @@ fn run_that_app_content_sorted() {
 fn feature_files(dir: &Path) -> Vec<PathBuf> {
     let mut files = Vec::new();
     collect_feature_files(dir, &mut files);
-    files.sort();
+    files.sort_unstable();
     files
 }
 
