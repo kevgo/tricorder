b19917151d8ffcf0d4dc817cf7d01e842ba46336
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-16 05:59:57 -0500

    runnables.rs

--- src/domain/runnables.rs
+++ src/domain/runnables.rs
@@ -9,16 +9,72 @@ pub struct Runnables {
 
 impl Runnables {
     #[must_use]
-    pub fn is_empty(&self) -> bool {
-        self.global.is_empty() && self.stack_specific.is_empty()
-    }
-
-    #[must_use]
     pub fn len(&self) -> usize {
         let mut result = self.global.len();
         for x in &self.stack_specific {
             result += x.len();
         }
         result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Runnables;
+
+    fn executable() -> conc::Executable {
+        conc::shell_executable("true")
+    }
+
+    mod len {
+        use super::{Runnables, executable};
+
+        #[test]
+        fn empty() {
+            let runnables = Runnables {
+                global: conc::Runnable::Sequence(vec![]),
+                stack_specific: vec![],
+            };
+            pretty::assert_eq!(runnables.len(), 0);
+        }
+
+        #[test]
+        fn single_global() {
+            let runnables = Runnables {
+                global: conc::Runnable::Single(executable()),
+                stack_specific: vec![],
+            };
+            pretty::assert_eq!(runnables.len(), 1);
+        }
+
+        #[test]
+        fn multiple_globals() {
+            let runnables = Runnables {
+                global: conc::Runnable::Sequence(vec![executable(), executable()]),
+                stack_specific: vec![],
+            };
+            pretty::assert_eq!(runnables.len(), 2);
+        }
+
+        #[test]
+        fn stack_specific() {
+            let runnables = Runnables {
+                global: conc::Runnable::Sequence(vec![]),
+                stack_specific: vec![
+                    conc::Runnable::Sequence(vec![executable()]),
+                    conc::Runnable::Sequence(vec![executable(), executable()]),
+                ],
+            };
+            pretty::assert_eq!(runnables.len(), 3);
+        }
+
+        #[test]
+        fn all_fields_set() {
+            let runnables = Runnables {
+                global: conc::Runnable::Sequence(vec![executable(), executable()]),
+                stack_specific: vec![conc::Runnable::Sequence(vec![executable()])],
+            };
+            pretty::assert_eq!(runnables.len(), 3);
+        }
     }
 }
