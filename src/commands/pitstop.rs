2c5bafdc8153d7ce06ec603631ade28907bacae7
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-16 05:27:35 -0500

    pitstop.rs

--- src/commands/pitstop.rs
+++ src/commands/pitstop.rs
@@ -38,16 +38,16 @@ pub(crate) fn run_fix_then_lint(
     }
 
     // step 1: discover the runnables
-    let fix_runnables = fix::determine_fixes(config, stacks)?;
+    let fixes = fix::determine_fixes(config, stacks)?;
     let lints = lint::determine_lints(config, stacks, repo)?;
-    let runnable_count = fix_runnables.len() + lints.len();
+    let tool_count = fixes.len() + lints.len();
     if show.display_metadata() {
-        eprintln!("running {runnable_count} tools");
+        eprintln!("running {tool_count} tools");
     }
     let Runnables {
         global: global_fixes,
         stack_specific: stack_specific_fixes,
-    } = fix_runnables;
+    } = fixes;
 
     // step 2: run the global fixes
     let exit_code = conc::run(conc::RunArgs {
