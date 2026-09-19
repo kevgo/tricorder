c94afb437de89cb79e9ea039f1e3889b6fc43b33
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-19 09:13:41 -0500

    lint.rs

--- src/commands/lint.rs
+++ src/commands/lint.rs
@@ -47,7 +47,7 @@ pub fn determine_lints(
     detected_stacks: &DetectedStacks,
     git_repo: Option<&git::Repo>,
 ) -> Result<Vec<conc::Sequence>> {
-    let mut result = Vec::new();
+    let mut result: Vec<conc::Executable> = Vec::new();
 
     // determine the lints for the stacks
     for detected_stack in detected_stacks {
@@ -56,24 +56,25 @@ pub fn determine_lints(
         // schedule either the override lints or the default lints
         let stack_lints = stack_config.and_then(|sc| sc.lint.as_ref());
         if let Some(overrides) = stack_lints.and_then(|lint| lint.replace.as_ref()) {
-            for override_lint in overrides {
-                let executable = override_lint.to_executable(Operation::Lint, stack_type);
-                result.push(conc::Sequence::one(executable));
-            }
+            result.extend(
+                overrides
+                    .iter()
+                    .map(|tool| tool.to_executable(Operation::Lint, stack_type)),
+            );
         } else {
             for default_lint in detected_stack.stack.lints() {
                 if config.operation_enabled(default_lint.as_ref(), Operation::Lint)
                     && default_lint.enabled_when().enabled_on_disk()
-                    && let Some(executable) = default_lint.lint_commands(detected_stack, config)?
+                    && let Some(sequence) = default_lint.lint_commands(detected_stack, config)?
                 {
-                    result.push(executable);
+                    result.extend(sequence);
                 }
             }
         }
         if let Some(additions) = stack_lints.and_then(|lint| lint.add.as_ref()) {
             for addition in additions {
                 let executable = addition.to_executable(Operation::Lint, stack_type);
-                result.push(conc::Sequence::one(executable));
+                result.push(executable);
             }
         }
     }
@@ -81,10 +82,10 @@ pub fn determine_lints(
     // determine the runnables for the custom lints
     if let Some(custom_lints) = &config.global_lints {
         for ToolDefinition { name, command } in custom_lints {
-            result.push(conc::Sequence::one(conc::Executable {
+            result.push(conc::Executable {
                 name: name.clone().unwrap_or_else(|| command.clone()),
                 command: conc::shell_command(command),
-            }));
+            });
         }
     }
 
@@ -93,8 +94,8 @@ pub fn determine_lints(
         && let Some(repo) = git_repo
     {
         let executable = git_diff_check::lint_command(repo);
-        result.push(conc::Sequence::one(executable));
+        result.push(executable);
     }
 
-    Ok(result)
+    Ok(result.into_iter().map(conc::Sequence::one).collect())
 }
