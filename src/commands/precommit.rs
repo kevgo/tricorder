47ea3e5e8144473320bb2ce0b7e4435d2bed86e2
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-19 07:08:10 -0500

    precommit.rs

--- src/commands/precommit.rs
+++ src/commands/precommit.rs
@@ -151,17 +151,15 @@ pub fn determine_precommit_fixes(
 
     // step 6: convert to runnables and return
     let mut stack_specific = Vec::new();
-    for (_stack_type, mut stack_executables) in stacks_executables {
-        if !stack_executables.is_empty() {
-            let first = stack_executables.remove(0);
-            stack_specific.push(conc::Sequence::many(first, stack_executables));
+    for (_stack_type, stack_executables) in stacks_executables {
+        if let Some(stack_sequences) = conc::Sequence::from_vec(stack_executables) {
+            stack_specific.push(stack_sequences);
         }
     }
     let global = if global.is_empty() {
         None
     } else {
-        let first = global.remove(0);
-        Some(conc::Sequence::many(first, global))
+        conc::Sequence::from_vec(global)
     };
     Ok(Runnables {
         global,
