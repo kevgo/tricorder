c69c9dcd9115f791cacb38804d330dc1763b99f8
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-23 06:33:33 -0500

    test.rs [skip ci]

--- src/commands/test.rs
+++ src/commands/test.rs
@@ -7,15 +7,15 @@ use std::process::ExitCode;
 pub fn test(args: &RunArgsWithTest) -> Result<ExitCode> {
     let config = Config::load()?;
     let show = args.run.show.unwrap_or(conc::Show::Names);
-    let requested = config.tests_for(&args.test, |commands| commands.test.as_ref());
-    let tests = config.select_requested_tests(requested)?;
+    let requested_tests = config.tests_for(&args.test, |commands| commands.test.as_ref());
+    let tests_to_use = config.select_requested_tests(requested_tests)?;
     if show.display_metadata() {
-        eprintln!("running {} tools", tests.len());
+        eprintln!("running {} tools", tests_to_use.len());
     }
-    if tests.is_empty() {
+    if tests_to_use.is_empty() {
         return Ok(ExitCode::SUCCESS);
     }
-    let test_sequences = to_sequences(tests);
+    let test_sequences = to_sequences(tests_to_use);
     let exit_code = conc::run(conc::RunArgs {
         sequences: test_sequences,
         error_on_output: false,
