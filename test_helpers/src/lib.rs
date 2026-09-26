1b1a48ab0da4ab00d703c5eacc18517c5e35e5ee
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-26 10:53:38 -0500

    lib.rs standardize_newlines.rs then_steps.rs [skip ci]

--- test_helpers/src/lib.rs
+++ test_helpers/src/lib.rs
@@ -5,6 +5,7 @@ mod has_additional_lines;
 mod remove_line_matching;
 mod remove_lines;
 pub mod snapshots;
+mod standardize_newlines;
 
 pub use compare_lines_any_order::compare_lines_any_order;
 use content_lines::content_lines;
@@ -12,3 +13,4 @@ pub use docstring_body::docstring_body;
 pub use has_additional_lines::has_additional_lines;
 use remove_line_matching::remove_line_matching;
 use remove_lines::remove_lines;
+use standardize_newlines::standardize_newlines;
